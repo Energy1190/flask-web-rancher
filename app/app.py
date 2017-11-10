5fabd6038e8d9860a468ffe423e104d5a4032939
Energy1190 <energyneo0@gmail.com>
2017-11-10 19:54:32 +0300

    0011
    Добавлена поддержка вывода json.

--- app/app.py
+++ app/app.py
@@ -32,14 +32,16 @@ def prepare_create(request):
                                                          status=baseerror.get('status'))
 
 def query_args(func):
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, redirected=False, **kwargs):
         args_r = {}
         if request.method == 'GET':
             args_r = {i: request.args.get(i) for i in list(request.args)}
             if args_r.get('rancher-url'): envs.env['RANCHER_API_URL'] = args_r.get('rancher-url')
-        if request.method == 'POST':
+            if 'application/json' in request.headers['Accept'].split(sep=' '): kwargs['answ_json'] = True
+        if request.method == 'POST' and not redirected:
             x = prepare_create(request)
-            if not x: x = list_stack(q_args=args_r, **kwargs)
+            if not x:
+                return list_stack(redirected=True, **kwargs)
         else:
             x = func(*args, q_args=args_r, **kwargs)
         return x
@@ -48,11 +50,12 @@ def query_args(func):
 
 @app.route("/", methods=['GET', 'POST'])
 @query_args
-def list_stack(q_args=None, **kwargs):
+def list_stack(q_args=None, answ_json=False, **kwargs):
     if not envs.app_env.get('RANCHER_API_URL'): return render_template('get_url.html')
     r = RancherAPI(key=envs.app_env.get('RANCHER_API_KEY'), secret=envs.app_env.get('RANCHER_API_SECRET'), base_url=envs.app_env.get('RANCHER_API_URL'))
     r.set_project()
     stacks =[(i.get('id'), i.get('name'), i.get('environment').get('SITEURL')) for i in r.get_stack_list().get('data') if i.get('group') == "io.rancher.service.create_by_app"]
+    if answ_json: return jsonify(stacks)
     return render_template('stack_list.html', stacks=stacks, base_url=envs.app_env.get('RANCHER_API_URL'))
 
 @app.route("/add", methods=['GET', 'POST'])
@@ -62,12 +65,13 @@ def add_stack(q_args=None, **kwargs):
 
 @app.route("/detail/<name>", methods=['GET'])
 @query_args
-def detail_stack(name, q_args=None, **kwargs):
+def detail_stack(name, q_args=None, answ_json=False, **kwargs):
     if not envs.app_env.get('RANCHER_API_URL'): return render_template('get_url.html')
     r = RancherAPI(key=envs.app_env.get('RANCHER_API_KEY'), secret=envs.app_env.get('RANCHER_API_SECRET'), base_url=envs.app_env.get('RANCHER_API_URL'))
     r.set_project()
     x = r.get_stack(name)
     detail = {i:x[i] for i in x if i in magic_list}
+    if answ_json: return jsonify((r, detail))
     return render_template('stack_detail.html', stack=r, detail=detail)
 
 @app.route("/delete/<name>", methods=['GET'])
@@ -87,4 +91,4 @@ def help_me(q_args=None, **kwargs):
     return render_template('help.html', help_me=x)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
