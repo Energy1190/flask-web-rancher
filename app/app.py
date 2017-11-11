ba84be03d9bc0c67b30eaedf0a20631c781a6076
Energy1190 <energyneo0@gmail.com>
2017-11-11 15:23:07 +0300

    add threaded

--- app/app.py
+++ app/app.py
@@ -90,4 +90,4 @@ def help_me(q_args=None, **kwargs):
     return render_template('help.html', help_me=x)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, threaded=True, processes=5)
